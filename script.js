8508889fc54855af6ead10138ff394ce7338e725
adityap5 <aditya29pippal@gmail.com>
2023-08-26 21:47:30 +0530

    animation added

---
+++ script.js
@@ -0,0 +1,25 @@
+gsap.from("#main #left-land",{
+    y:70,
+    duration:1,
+    delay:.5,
+})
+gsap.from("#main #left-po",{
+    x:-100,
+    duration:1,
+    delay:.5,
+})
+gsap.from("#main #right-land",{
+    y:-200,
+    duration:1,
+    delay:.5,
+})
+gsap.from("#main #right-po",{
+    x:120,
+    duration:1,
+    delay:.5,
+})
+gsap.from("#nav img",{
+    y:-100,
+    duration:1,
+    delay:.5,
+})
