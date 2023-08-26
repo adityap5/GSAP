43a4bdcd0e43823d30366bbd6e1b6d080e11a067
adityap5 <aditya29pippal@gmail.com>
2023-08-26 21:58:22 +0530

    fixed animation

--- script.js
+++ script.js
@@ -2,24 +2,35 @@ gsap.from("#main #left-land",{
     y:70,
     duration:1,
     delay:.5,
+    opacity:0
 })
 gsap.from("#main #left-po",{
     x:-100,
     duration:1,
     delay:.5,
+    opacity:0
 })
-gsap.from("#main #right-land",{
-    y:-200,
-    duration:1,
-    delay:.5,
-})
-gsap.from("#main #right-po",{
+// gsap.from("#main #right-land,",{
+//     y:-200,
+//     duration:1,
+//     delay:.5,
+//     opacity:0
+// })
+gsap.from("#main #right-po,#main #right-land",{
     x:120,
     duration:1,
     delay:.5,
+    opacity:0
 })
-gsap.from("#nav img",{
+gsap.from("#nav img,#nav h3 ,#nav h4",{
     y:-100,
     duration:1,
     delay:.5,
+    opacity:0
+})
+gsap.from("#nav button",{
+    x:-100,
+    duration:1,
+    delay:.5,
+    opacity:0
 })
